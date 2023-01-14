1dc3bb935cc04a2f5f2a0e5e2326cfade9e410d3
AdarshG930 <itsadarshofficial1@gmail.com>
2023-01-14 23:31:46 +0530

    completed the BeatBox project

--- BeatBoxProject/script.js
+++ BeatBoxProject/script.js
@@ -16,40 +16,55 @@
  */
 
 let beats = {
-    "65": {
-        beat: new Beat("./assets/Piano Chord 331.mp3"),
-    },
-    "83": {
-        beat: new Beat("./assets/Piano Chord 209.mp3"),
-    },
-    "68": {
-        beat: new Beat("./assets/Piano Chord 208.mp3"),
-    },
-    "70": {
-        beat: new Beat("./assets/Drum Sticks Hit 3.mp3"),
-    },
-    "71": {
-        beat: new Beat("./assets/Drum Snare Roll.mp3"),
-    },
-    "72": {
-        beat: new Beat("./assets/PREL Musical 57.mp3"),
-    },
-    "74": {
-        beat: new Beat("./assets/Cymbal Suspended 2.mp3"),
-    },
-    "75": {
-        beat: new Beat("./assets/Musical Compos 33.mp3"),
-    },
-    "76": {
-        beat: new Beat("./assets/Musical Orches 4.mp3"),
-    }
-}
+  65: {
+    beat: new Beat("./assets/Piano Chord 331.mp3"),
+    button: new Button("#00fffe", 65),
+  },
+  83: {
+    beat: new Beat("./assets/Piano Chord 209.mp3"),
+    button: new Button("#00fffe", 83),
+  },
+  68: {
+    beat: new Beat("./assets/Piano Chord 208.mp3"),
+    button: new Button("#00fffe", 68),
+  },
+  70: {
+    beat: new Beat("./assets/Drum Sticks Hit 3.mp3"),
+    button: new Button("#FF00FF", 70),
+  },
+  71: {
+    beat: new Beat("./assets/Drum Snare Roll.mp3"),
+    button: new Button("#FF00FF", 71),
+  },
+  72: {
+    beat: new Beat("./assets/PREL Musical 57.mp3"),
+    button: new Button("#FF00FF", 72),
+  },
+  74: {
+    beat: new Beat("./assets/Cymbal Suspended 2.mp3"),
+    button: new Button("#FF00FF", 74),
+  },
+  75: {
+    beat: new Beat("./assets/Musical Compos 33.mp3"),
+    button: new Button("#FFFFFF", 75),
+  },
+  76: {
+    beat: new Beat("./assets/Musical Orches 4.mp3"),
+    button: new Button("#FFFFFF", 76),
+  },
+};
 
 /**
  * Function to play the beat upon a press of key
  * HINT: use the keyCode
  */
 triggerBeat = (event) => {
+    const keyCode  = event.keyCode;
+    if(keyCode in beats){
+        let pressKey = beats[keyCode];
+        pressKey.beat.play();
+        pressKey.button.select();
+    }
 
 }
 
@@ -57,3 +72,5 @@ triggerBeat = (event) => {
  * Keydown listener to fire triggerBeat function
  * HINT: Log the keyCode of the key
  */
+
+document.addEventListener('keydown',triggerBeat);
--- BeatBoxProject/utils.js
+++ BeatBoxProject/utils.js
@@ -4,6 +4,14 @@
  * HINT: Create a play function to play the audio if called 
  */
 class Beat {
+    constructor(audioSrc){
+        this.audio = new Audio(audioSrc);
+    }
+
+    play= () => {
+        this.audio.currentTime= 0;
+        this.audio.play();
+    }
 }
 
 
@@ -13,23 +21,38 @@ class Beat {
  */
 class Button {
     constructor(color, keyCode){
+        this.color= color;
+        this.keyCode= keyCode;
+        this.element = document.getElementById(keyCode);
+        this.setButtonColorInHTML();
+        this.setATransitionEndListener();
+    }
+
+    setATransitionEndListener= () =>{
+        this.element.addEventListener("transitionend", ()=>{
+            this.deselect();
+        })
     }
 
     /**
      * Set the button color based on color specified
      */
     setButtonColorInHTML = () => {
+       this.element.style.borderColor= this.color;
     }
 
     /**
      * Select function to set the background color and boxShadow
      */
     select = () => {
+        this.element.style.backgroundColor= this.color;
+        this.element.style.boxShadow = `0px 0px 17px 0px ${this.color}`;
     }
-
     /**
      * Deselect function to reset background color and boxShadow
      */
     deselect = () => {
+        this.element.style.backgroundColor = "transparent";
+        this.element.style.boxShadow = "none";
     }
 }
